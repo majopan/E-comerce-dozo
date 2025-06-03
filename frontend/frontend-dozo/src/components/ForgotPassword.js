73fd042525f2eea7c69d464b7ecf766a1b16e3d9
Maria Jose Jimenez Diaz <mariazzz2326@gmail.com>
2025-06-02 23:51:28 -0500

    Update ForgotPassword.js

--- frontend/frontend-dozo/src/components/ForgotPassword.js
+++ frontend/frontend-dozo/src/components/ForgotPassword.js
@@ -16,7 +16,7 @@ const ForgotPassword = () => {
 
         try {
             // Enviar solicitud al backend para enviar correo
-            const response = await axios.post('https://dozo01.pythonanywhere.com/api/reset-password/', { email });
+            const response = await axios.post('https://dozo01.pythonanywhere.com/api/send-reset-email/', { email });
 
             // Mostrar mensaje de éxito y redirigir al usuario
             setSuccessMessage(response.data.message || 'Correo enviado con éxito.');
